42e0eb2512f1a85ebd8e48afa695189df62babbc
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-25 19:15:35 -0500

    wip: setting up nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Header = (props) => {
-  return <h1>Header</h1>;
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div>repoPortfolio</div>
+      </Link>
+    </nav>
+  );
 };
 
 export default Header;
