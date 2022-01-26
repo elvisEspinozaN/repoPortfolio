6f395d984d5b3c00a89ad0a859e07bc75b5cdd84
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-25 20:23:22 -0500

    wip: import to set state/url added/

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,14 @@
 import { Route, Switch } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 const Main = (props) => {
+  const [repo, setRepo] = useState([]);
+
+  const URL = "http://localhost:3001/repo/";
+  // const URL = "https://repo-portfolio.herokuapp.com/repo/";
+
   return (
     <main>
       <Switch>
