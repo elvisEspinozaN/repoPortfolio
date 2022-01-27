bf52c2cb85d7fa641fdf3ffc71c4364de237747c
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-27 00:29:58 -0500

    wip: displaying repos

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,7 +4,12 @@ const Index = (props) => {
   const loaded = () => {
     return props.repo.map((repo) => (
       <div key={repo._id} className="repo">
-        <Link></Link>
+        <h4>repo.author</h4>
+        <h3>repo.pName</h3>
+        <Link to={`/repo/${repo._id}`}>
+          <h3>repo.url</h3>
+        </Link>
+        <h3>repo.progress</h3>
       </div>
     ));
   };
