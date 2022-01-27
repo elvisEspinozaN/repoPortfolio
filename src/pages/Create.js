6890f7889cb642b85bfa28741231ac0f85f2fabc
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-27 16:33:52 -0500

    wip: form to create repo

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 //
-const Create = (props) => {};
+const Create = (props) => {
+  // state that hold form data
+  const [form, setForm] = useState(
+    {
+      author: "",
+      pName: "",
+      progress: "",
+      url: "",
+    },
+    { timestamp: true }
+  );
+};
 
 export default Create;
