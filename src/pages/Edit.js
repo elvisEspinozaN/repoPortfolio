668cd3747552645865ae4499bd65380a863de6c0
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-27 19:44:31 -0500

    wip: state in form set

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
-const Edit = (props) => {};
+const Edit = (props) => {
+  const id = props.match.params.id;
+  const repo = props.repo;
+  const repos = repo.find((p) => p._id === id);
+
+  const [edit, setEdit] = useState(repos);
+};
 
 export default Edit;
