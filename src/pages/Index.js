6670c300968a891eb96ffd5c01d69db9a2c8b02d
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-27 19:00:51 -0500

    wip: small changes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,7 +4,7 @@ const Index = (props) => {
   const loaded = () => {
     return props.repo.map((repo) => (
       <div key={repo._id} className="repo">
-        <h4>repo.author</h4>
+        <h3>repo.author</h3>
         <h3>repo.pName</h3>
         <Link to={`/repo/${repo._id}`}>
           <h3>repo.url</h3>
