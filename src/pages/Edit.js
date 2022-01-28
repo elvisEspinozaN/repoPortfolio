e3c3c5182a04b3baa2d3a36631e9cec553f2a502
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-27 19:48:25 -0500

    wip: change handler logic

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -6,6 +6,14 @@ const Edit = (props) => {
   const repos = repo.find((p) => p._id === id);
 
   const [edit, setEdit] = useState(repos);
+
+  // handle change
+  const changeHandler = (evt) => {
+    setEdit((prevState) => ({
+      ...prevState,
+      [evt.target.name]: evt.target.value,
+    }));
+  };
 };
 
 export default Edit;
